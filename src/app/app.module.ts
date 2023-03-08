52405bc6ee7b1a31297d37c71fc8153a06581e37
carluzox1999 <carlosescolarcalero@alumno.ieselrincon.es>
2023-03-08 17:21:49 +0000

    CARLOS: Creación de un componente
    Creación del componente "profile" para empezar con el proyecto. Primeros pasos en el proceso de la aplicación para entender como funcionan los componentes. Y los archivos más importantes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
