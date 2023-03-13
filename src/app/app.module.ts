d6a21222a771e8186d3a16e9d913585dbb5f995a
carluzox1999 <carlosescolarcalero@alumno.ieselrincon.es>
2023-03-13 18:41:37 +0000

    CARLOS: Últimos puntos
    Publicación del proyecto en GitHub para que me de un token especial y así me de más resultados al usar la api. Completado el menú de la aplicación y su responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -7,20 +8,27 @@ import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { appRouterProviders, routing } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    CollapseModule.forRoot(),
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    routing
   ],
-  providers: [],
+  providers: [ appRouterProviders ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/github/github.service.ts
+++ src/app/services/github/github.service.ts
@@ -7,18 +7,20 @@ import { map } from 'rxjs/operators';
 })
 export class GithubService {
   private username: string;
+  private client_id = '718afbc8f75f826c5658';
+  private client_secret = 'cea932fd5b52a21afbb960b47a7f3e1de14c2704';
 
   constructor(private _http: HttpClient )  {
       this.username = 'samuel830';
   }
 
   getUser()  {
-      return this._http.get('https://api.github.com/users/' + this.username)
+      return this._http.get('https://api.github.com/users/' + this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
         .pipe(map(res => res));
   }
 
   getRepos(){
-      return this._http.get('https://api.github.com/users/' + this.username + '/repos')
+      return this._http.get('https://api.github.com/users/' + this.username + '/repos'+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
         .pipe(map(res => res));
   }
 
